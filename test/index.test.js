3c4b3eb82cca0089ee74b0c10d53e7f2bd4df815
Patrick Hulce <patrick.hulce@gmail.com>
2017-02-20 14:43:53 -0800

    test: add basic tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,46 @@
-// const Plugin = require('../lib')
+const fs = require('fs')
+const _ = require('lodash')
+const webpack = require('webpack')
 
 describe('NukeCssPlugin', function () {
-  describe('FontAwesome', function () {
-    it('should work', function () {
-      // something.should.equal(2);
+  let fileStats
+  const baseConfig = require('./fixtures/webpack.config.js')
+
+  function collectFileStats(stats) {
+    return _(stats.compilation.assets)
+      .keys()
+      .map(filename => {
+        const fullPath = `${__dirname}/fixtures/dist/${filename}`
+        return {
+          filename,
+          stats: fs.statSync(fullPath),
+          content: fs.readFileSync(fullPath, 'utf8'),
+        }
+      })
+      .keyBy('filename')
+      .value()
+  }
+
+  function testWithConfig(config, done) {
+    webpack(config, (err, stats) => {
+      if (err) {
+        done(err)
+      } else {
+        fileStats = collectFileStats(stats)
+        done()
+      }
     })
+  }
+
+  before(done => testWithConfig(baseConfig, done))
+
+  it('should work with style-loader', function () {
+    expect(fileStats['out.js'].content).to.contain('.fa-address-book-o')
+    expect(fileStats['out.js'].content).to.not.contain('.my-favorite-class')
+  })
+
+  it('should work with ExtractTextPlugin', function () {
+    expect(fileStats['out.css'].content).to.contain('.fa-address-book-o')
+    expect(fileStats['out.css'].content).to.not.contain('.my-favorite-class')
   })
 })
